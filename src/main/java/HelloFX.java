254a5a19ce1478ccb99f087eae39faa70fb225f5
px2ku <54834452+px2ku@users.noreply.github.com>
2021-04-01 23:35:04 +0200

    changed HelloFX.java

--- src/main/java/HelloFX.java
+++ src/main/java/HelloFX.java
@@ -10,7 +10,7 @@ public class HelloFX extends Application {
     public void start(Stage stage) {
         String javaVersion = System.getProperty("java.version");
         String javafxVersion = System.getProperty("javafx.version");
-        Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
+        Label l = new Label("Dazai" + javafxVersion + ", running on Java " + javaVersion + ".");
         Scene scene = new Scene(new StackPane(l), 640, 480);
         stage.setScene(scene);
         stage.show();
